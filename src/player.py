277e6c5601fae5ab41d3153e460b2aa605d9605c
pelayolopez <pelayo.lopez@udc.es>
2025-02-15 01:38:48 +0100

    Correccion en el movimiento del tanque, y correccion de pequeño bug en el apuntado del cañon con el raton

--- src/player.py
+++ src/player.py
@@ -21,6 +21,8 @@ class Player:
         self.ancho_pantalla, self.alto_pantalla = pantalla.get_size()
         self.image = self.sprites["abajo"]
         self.rect = self.image.get_rect(center=(self.ancho_pantalla // 2 + self.ancho_pantalla, self.alto_pantalla // 2 + self.alto_pantalla))
+        self.hitbox = pygame.Rect(self.ancho_pantalla // 2 + self.ancho_pantalla, self.alto_pantalla // 2 + self.alto_pantalla, self.rect.width * 0.8, self.rect.height * 0.8)
+
         self.velocidad = 3
         self.direccion = "abajo"  # Dirección inicial del tanque
         self.balas = []  # Lista para almacenar las balas disparadas
@@ -33,50 +35,73 @@ class Player:
     def update(self, pantalla, mundo):
         teclas = pygame.key.get_pressed()
         direccion = None
-        nuevo_rect = self.rect.copy()
+        nuevo_hitbox = self.hitbox.copy()
 
-        if (teclas[pygame.K_LEFT] or teclas[pygame.K_a]) and (teclas[pygame.K_UP] or teclas[pygame.K_w]):
-            nuevo_rect.x -= self.velocidad * 0.707
-            nuevo_rect.y -= self.velocidad * 0.707
-            direccion = "arriba_izquierda"
-        elif (teclas[pygame.K_RIGHT] or teclas[pygame.K_d]) and (teclas[pygame.K_UP] or teclas[pygame.K_w]):
-            nuevo_rect.x += self.velocidad * 0.707
-            nuevo_rect.y -= self.velocidad * 0.707
-            direccion = "arriba_derecha"
-        elif (teclas[pygame.K_LEFT] or teclas[pygame.K_a]) and (teclas[pygame.K_DOWN] or teclas[pygame.K_s]):
-            nuevo_rect.x -= self.velocidad * 0.707
-            nuevo_rect.y += self.velocidad * 0.707
-            direccion = "abajo_izquierda"
-        elif (teclas[pygame.K_RIGHT] or teclas[pygame.K_d]) and (teclas[pygame.K_DOWN] or teclas[pygame.K_s]):
-            nuevo_rect.x += self.velocidad * 0.707
-            nuevo_rect.y += self.velocidad * 0.707
-            direccion = "abajo_derecha"
-        elif teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
-            nuevo_rect.x -= self.velocidad
-            direccion = "izquierda"
-        elif teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
-            nuevo_rect.x += self.velocidad
-            direccion = "derecha"
-        elif teclas[pygame.K_UP] or teclas[pygame.K_w]:
-            nuevo_rect.y -= self.velocidad
-            direccion = "arriba"
-        elif teclas[pygame.K_DOWN] or teclas[pygame.K_s]:
-            nuevo_rect.y += self.velocidad
-            direccion = "abajo"
+        # Movimiento en X e Y por separado
+        movimiento_x = 0
+        movimiento_y = 0
+
+        if teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
+            movimiento_x = -self.velocidad
+        if teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
+            movimiento_x = self.velocidad
+        if teclas[pygame.K_UP] or teclas[pygame.K_w]:
+            movimiento_y = -self.velocidad
+        if teclas[pygame.K_DOWN] or teclas[pygame.K_s]:
+            movimiento_y = self.velocidad
+
+        # Movimiento en diagonal (se reduce velocidad)
+        if movimiento_x != 0 and movimiento_y != 0:
+            movimiento_x *= 0.707  # √2/2 para mantener velocidad uniforme
+            movimiento_y *= 0.707
+
+        # --- Verificar colisiones por separado ---
+        colision_x = False
+        colision_y = False
+
+        # 1. Mover en X y verificar colisión
+        nuevo_hitbox.x += movimiento_x
+        if any(nuevo_hitbox.colliderect(elemento.rect) for elemento in mundo.elementos):
+            nuevo_hitbox.x -= movimiento_x  # Si hay colisión, deshacer movimiento en X
+            colision_x = True
+
+        # 2. Mover en Y y verificar colisión
+        nuevo_hitbox.y += movimiento_y
+        if any(nuevo_hitbox.colliderect(elemento.rect) for elemento in mundo.elementos):
+            nuevo_hitbox.y -= movimiento_y  # Si hay colisión, deshacer movimiento en Y
+            colision_y = True
 
         # Verificar si el jugador sale de la pantalla y cambiar la cámara
-        if nuevo_rect.right > mundo.camara_x + mundo.ancho_pantalla + 50:
+        if nuevo_hitbox.right > mundo.camara_x + mundo.ancho_pantalla + 50:
             mundo.cambiar_pantalla("derecha")
-        elif nuevo_rect.left < mundo.camara_x - 50:
+        elif nuevo_hitbox.left < mundo.camara_x - 50:
             mundo.cambiar_pantalla("izquierda")
-        elif nuevo_rect.bottom > mundo.camara_y + mundo.alto_pantalla + 50:
+        elif nuevo_hitbox.bottom > mundo.camara_y + mundo.alto_pantalla + 50:
             mundo.cambiar_pantalla("abajo")
-        elif nuevo_rect.top < mundo.camara_y - 50:
+        elif nuevo_hitbox.top < mundo.camara_y - 50:
             mundo.cambiar_pantalla("arriba")
 
-        # Si no hubo cambio de pantalla, verificar colisiones y mover jugador
-        if not any(nuevo_rect.colliderect(elemento.rect) for elemento in mundo.elementos):
-            self.rect = nuevo_rect
+        # --- Determinar la dirección final según los movimientos permitidos ---
+        if movimiento_x < 0 and movimiento_y < 0 and not colision_x and not colision_y:
+            direccion = "arriba_izquierda"
+        elif movimiento_x > 0 and movimiento_y < 0 and not colision_x and not colision_y:
+            direccion = "arriba_derecha"
+        elif movimiento_x < 0 and movimiento_y > 0 and not colision_x and not colision_y:
+            direccion = "abajo_izquierda"
+        elif movimiento_x > 0 and movimiento_y > 0 and not colision_x and not colision_y:
+            direccion = "abajo_derecha"
+        elif movimiento_x < 0 and not colision_x:
+            direccion = "izquierda"
+        elif movimiento_x > 0 and not colision_x:
+            direccion = "derecha"
+        elif movimiento_y < 0 and not colision_y:
+            direccion = "arriba"
+        elif movimiento_y > 0 and not colision_y:
+            direccion = "abajo"
+
+        # Aplicar el movimiento final
+        self.hitbox = nuevo_hitbox
+        self.rect.center = self.hitbox.center  # Asegurar que el sprite coincida con la hitbox
 
         if direccion:
             self.image = self.sprites[direccion]
@@ -99,8 +124,8 @@ class Player:
         cursorx, cursory = pygame.mouse.get_pos()
 
         # Calcular el ángulo hacia el cursor
-        diff_x = cursorx - (self.rect.x - mundo.camara_x)
-        diff_y = cursory - (self.rect.y - mundo.camara_y)
+        diff_x = cursorx - (self.rect.centerx - mundo.camara_x)
+        diff_y = cursory - (self.rect.centery - mundo.camara_y)
         angle = numpy.degrees(numpy.arctan2(diff_y, diff_x)) + 270  # Calcular ángulo en grados
 
         # Rotar la imagen del cañón
@@ -125,3 +150,6 @@ class Player:
         self.update_cannon_position(mundo)
 
         pantalla.blit(self.top_image, (self.rect.centerx - self.rec.width // 2 - mundo.camara_x, self.rect.centery - self.rec.height // 2 - mundo.camara_y))
+
+        #Mostrar la Hitbox
+        #pygame.draw.rect(pantalla, (255, 0, 0), (self.hitbox.x - mundo.camara_x, self.hitbox.y - mundo.camara_y, self.hitbox.width, self.hitbox.height), 2)
