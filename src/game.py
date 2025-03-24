e873114d875854ee2384e1c8fb4c3777411998df
rafael-leite03 <114733839+rafael-leite03@users.noreply.github.com>
2025-03-24 14:38:37 -0300

    game.py inicia en el menu inicial

--- src/game.py
+++ src/game.py
@@ -1,22 +1,21 @@
 import pygame
 
 from .director import Director
-from .worlds.world1 import World1
+from .menus.menu import MainMenu
+
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.tamaño_pantalla = 1
 
         self.director = Director()
 
         """if evento.key == pygame.K_F11:
                             self.pantalla = pygame.display.set_mode((ANCHO, ALTO))""" #para cambiar la pantalla
 
-        self.mundo1 = World1(self.director.pantalla.get_height(), self.director.pantalla.get_width())
+        escena = MainMenu(self.director)
+        self.director.cambiar_escena(escena)
 
-        self.director.cambiar_escena(self.mundo1)
 
     def run(self):
         self.director.ejecutar()
-
